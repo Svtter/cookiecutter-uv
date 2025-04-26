0c1d651becd66a576c5f4bc9a373d0d494a124e2
svtter <svtter@qq.com>
2025-04-26 10:40:49 +0800

    format code.

--- cookiecutter_uv/cli.py
+++ cookiecutter_uv/cli.py
@@ -4,6 +4,6 @@ import os
 
 
 def main() -> None:
-    cwd = os.path.dirname(__file__)
-    package_dir = os.path.abspath(os.path.join(cwd, ".."))
-    os.system(f"cookiecutter {package_dir}")  # noqa: S605 | No injection, retrieving path in OS
+  cwd = os.path.dirname(__file__)
+  package_dir = os.path.abspath(os.path.join(cwd, ".."))
+  os.system(f"cookiecutter {package_dir}")  # noqa: S605 | No injection, retrieving path in OS
--- tests/utils.py
+++ tests/utils.py
@@ -8,35 +8,35 @@ import yaml
 
 
 def is_valid_yaml(path: str | Path):
-    path = Path(path)
+  path = Path(path)
 
-    if not path.is_file():
-        print(f"File does not exist: {path}")
-        return False
+  if not path.is_file():
+    print(f"File does not exist: {path}")
+    return False
 
-    try:
-        with path.open("r") as file:
-            yaml.safe_load(file)
-    except yaml.YAMLError as e:
-        print(f"Invalid YAML file: {path} - Error: {e}")
-        return False
-    except OSError as e:
-        print(f"Error reading file: {path} - Error: {e}")
-        return False
+  try:
+    with path.open("r") as file:
+      yaml.safe_load(file)
+  except yaml.YAMLError as e:
+    print(f"Invalid YAML file: {path} - Error: {e}")
+    return False
+  except OSError as e:
+    print(f"Error reading file: {path} - Error: {e}")
+    return False
 
-    return True
+  return True
 
 
 @contextmanager
 def run_within_dir(path: str):
-    oldpwd = os.getcwd()
-    os.chdir(path)
-    try:
-        yield
-    finally:
-        os.chdir(oldpwd)
+  oldpwd = os.getcwd()
+  os.chdir(path)
+  try:
+    yield
+  finally:
+    os.chdir(oldpwd)
 
 
 def file_contains_text(file: str, text: str) -> bool:
-    with open(file) as f:
-        return f.read().find(text) != -1
+  with open(file) as f:
+    return f.read().find(text) != -1
